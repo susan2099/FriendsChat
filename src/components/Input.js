8922e46a97b4aebed2dce266ddec95421a6a0019
susan2099 <59705471+susan2099@users.noreply.github.com>
2024-02-25 16:18:32 -0800

    styled chat box

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import Img from "../images/img.png"
 
 const Input = () => {
   return (
-    <div className='input'> Input
+    <div className='input'>
       <input type="text" placeholder='Type something...'/>
       <div className='send'>
         <input type="file" style={{display:"none"}} id="file"/>
