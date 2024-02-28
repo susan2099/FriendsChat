1efd76aa5e97f62c2ccaea5a3fe03467cf54ae8a
susan2099 <59705471+susan2099@users.noreply.github.com>
2024-02-27 20:29:56 -0800

    updated message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img 
+          src='https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg' 
+          alt=''
+        />
+        <span>Just Now</span>
+      </div> 
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src='https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg' alt=''/>
+      </div>
+    </div>
+  );
+};
 
 export default Message
